f86312245fb6ece7a34e6e36ed035c89541eb73c
ANDRIANAIVO Ismael Blaise <152376566+ismaelBlaise@users.noreply.github.com>
2026-02-03 23:15:59 +0300

    feat:creation des header et footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./layouts/_layout";
+import { ActiveTabProvider } from "./contexts/ActiveTabContext";
 
 function App() {
   return (
-    <>
+    <ActiveTabProvider>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Layout />}></Route>
         </Routes>
       </BrowserRouter>
-    </>
+    </ActiveTabProvider>
   );
 }
 
